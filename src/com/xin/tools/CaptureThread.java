e736c1de74a6e71dac4aa9e64856025a7410339f
zhangxiaolong <zhangxiaolong@bogon>
2018-02-03 17:57:05 +0800

    修改捕获图片后的存储路径，由绝对路径改为相对路径。

--- src/com/xin/tools/CaptureThread.java
+++ src/com/xin/tools/CaptureThread.java
@@ -1,17 +1,17 @@
 package com.xin.tools;
 
 import java.text.SimpleDateFormat;
-
+import java.io.File;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamUtils;
 import com.github.sarxos.webcam.util.ImageUtils;
 
 public class CaptureThread extends Thread {
-	
-	private static String currentTime;
+
 	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH-mm-ss-S");
-	private static final String url = "D://";
+	private static String currentTime = dateFormat.format(System.currentTimeMillis());
+	private static final String url = "../../logs";
 	private static String fileName;
 	private Webcam webcam = Webcam.getDefault();
 	private WebcamPanel panel = new WebcamPanel(webcam);
@@ -22,6 +22,7 @@ public class CaptureThread extends Thread {
 		panel = new WebcamPanel(webcam);
 		panel.setImageSizeDisplayed(true);
 		panel.setMirrored(true);
+		fileIsExists(url);//构造时调用，保证存储路径正确
 	}
 
 	/*
@@ -30,16 +31,27 @@ public class CaptureThread extends Thread {
 	public void run(){	
 		//调用线程时启动拍照，直到线程受到干预而结束
 		while(true){
-			currentTime = dateFormat.format(System.currentTimeMillis());//获取当前系统时间并格式化
+			//currentTime = dateFormat.format(System.currentTimeMillis());//获取当前系统时间并格式化
 			fileName = url+currentTime;//存放图片
 			WebcamUtils.capture(webcam, fileName, ImageUtils.FORMAT_PNG);
 		}
 	}
-	
+	//获取实例对象引用
 	public Webcam getWebcam(){
 		return this.webcam;
 	}
 	public WebcamPanel getWebcamPanel(){
 		return this.panel;
+	}
+	
+	//判断文件路径/文件是否存在
+	public static boolean fileIsExists(String path) {
+		File file = new File(path);
+		if(!file.exists()) {
+			file.mkdirs();
+			return true;
+		}else {
+			return true;
+		}
 	}
 }
